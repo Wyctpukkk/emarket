fc840b8c836b8c0b98d97abd9e8e8381cdaa1c2b
Vitaly <poroshenkoe@gmail.com>
2023-03-21 04:34:52 +0300

    add: cards html+css structure

--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -1,7 +1,85 @@
-import React from 'react';
-
 const Cards = () => {
-  return <div></div>;
+  return (
+    <div className="cards">
+      <ul className="cards__list">
+        <li className="card">
+          <div className="card__image-wrapper">
+            <img className="card__image" alt="product" />
+          </div>
+          <p className="card__desc">
+            <span className="card__title">AOS</span> средство для мытья посуды
+            Crystal
+          </p>
+          <ul className="card__details">
+            <li className="card__details_item">
+              Штрихкод:<span>4604049097548</span>
+            </li>
+            <li className="card__details_item">
+              Производитель:<span>Нэфис</span>
+            </li>
+            <li className="card__details_item">
+              Бренд:<span>АOS</span>
+            </li>
+          </ul>
+          <div className="card__payload">
+            <span className="card__cost">48,76 ₸</span>
+            <button className="card__add">В КОРЗИНУ</button>
+            <span className="card__add_icon"></span>
+          </div>
+        </li>
+        <li className="card">
+          <div className="card__image-wrapper">
+            <img className="card__image" alt="product" />
+          </div>
+          <p className="card__desc">
+            <span className="card__title">AOS</span> средство для мытья посуды
+            Crystal
+          </p>
+          <ul className="card__details">
+            <li className="card__details_item">
+              Штрихкод:<span>4604049097548</span>
+            </li>
+            <li className="card__details_item">
+              Производитель:<span>Нэфис</span>
+            </li>
+            <li className="card__details_item">
+              Бренд:<span>АOS</span>
+            </li>
+          </ul>
+          <div className="card__payload">
+            <span className="card__cost">48,76 ₸</span>
+            <button className="card__add">В КОРЗИНУ</button>
+            <span className="card__add_icon"></span>
+          </div>
+        </li>
+        <li className="card">
+          <div className="card__image-wrapper">
+            <img className="card__image" alt="product" />
+          </div>
+          <p className="card__desc">
+            <span className="card__title">AOS</span> средство для мытья посуды
+            Crystal
+          </p>
+          <ul className="card__details">
+            <li className="card__details_item">
+              Штрихкод:<span>4604049097548</span>
+            </li>
+            <li className="card__details_item">
+              Производитель:<span>Нэфис</span>
+            </li>
+            <li className="card__details_item">
+              Бренд:<span>АOS</span>
+            </li>
+          </ul>
+          <div className="card__payload">
+            <span className="card__cost">48,76 ₸</span>
+            <button className="card__add">В КОРЗИНУ</button>
+            <span className="card__add_icon"></span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export { Cards };
